203d031c02d6ea010bac8618fb20356a288509e0
Arman <armanshoraka@gmail.com>
2020-09-29 11:32:32 -0400

    dropdowns and facts

--- src/body/UnitedStates.js
+++ src/body/UnitedStates.js
@@ -6,6 +6,7 @@ import {
 } from "../api/datacollection";
 import USMap from "../image/download.png";
 import "./UnitedStates.scss";
+import DropDown from "./dropdown";
 
 function UnitedStates() {
   const [confirmed, setconfirmed] = useState("0");
@@ -16,15 +17,55 @@ function UnitedStates() {
     getUSDataDead().then(setDead);
     getUSDataRecovered().then(setRecovered);
   }, []);
+
+  const deathCount911 = 2977;
+  const polioDeathCount = 3145;
+  const swineFluDeathCount = 12469;
+  const choleraDeathCount = 12000;
+
   return (
     <div className="usContainer">
       <div className="usmap">
         <img src={USMap} alt="" />
         <div className="info">
-          <p className="status confirmed">Confirmed: {confirmed}</p>
-          <p className="status dead">Dead: {dead}</p>
-          <p className="status recovered">Recovered: {recovered}</p>
+          <DropDown />
+          <p className="status confirmed">
+            Confirmed: <br />
+            {confirmed}
+          </p>
+          <p className="status dead">
+            Dead: <br />
+            {dead}
+          </p>
+          <p className="status recovered">
+            Recovered: <br />
+            {recovered}
+          </p>
         </div>
+      </div>
+      <div className="facts">
+        <h1 className="factsTitle">Relative Severity Facts</h1>
+        <ul>
+          <li>
+            The death count of CoronaVirus is equivalent to{" "}
+            {parseInt(dead / deathCount911)} Septermber 11 deaths.
+          </li>
+          <li>
+            The death count of CoronaVirus is equivalent to{" "}
+            {parseInt(dead / polioDeathCount)} times the number of people who
+            suffered and passed away painfully from Polio.
+          </li>
+          <li>
+            The death count of CoronaVirus is equivalent to having{" "}
+            {parseInt(dead / swineFluDeathCount)} swine flu (H1N1) outbreaks
+            back to back.
+          </li>
+          <li>
+            The death count of CoronaVirus is equivalent to{" "}
+            {parseInt(dead / choleraDeathCount)} times the people who died to
+            cholera..
+          </li>
+        </ul>
       </div>
     </div>
   );
